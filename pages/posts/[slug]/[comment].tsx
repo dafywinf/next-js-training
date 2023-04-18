3c95c78e454844b1604e842fda9eb58ae817102a
Dafydd Winfield <dafydd.winfield@baesystems.com>
2023-04-18 07:26:33 +0100

    Adding dynamic routing at page and directory level

---
+++ pages/posts/[slug]/[comment].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+
+
+export default function Comment() {
+    const router = useRouter();
+    const query = router.query;
+
+    return (
+        <div>Post: {query.slug}, Comment: {query.comment}</div>
+    )
+}

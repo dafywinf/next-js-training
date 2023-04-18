4305cd828b8f3819131838dc4253c6222c44ab2e
Dafydd Winfield <dafydd.winfield@baesystems.com>
2023-04-18 07:48:58 +0100

    Adding site-map and links for all URLs

---
+++ pages/site-map.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+
+
+export default function SiteMap() {
+
+    const pageIds = [1, 2, 3, 4];
+
+    return (
+        <>
+            <div>Site Map</div>
+            <ul>
+                <li>
+                    <Link href="/about">Go to: /about.tsx</Link>
+                </li>
+                <li>
+                    <Link href="/site-map">Go to: /site-map.tsx</Link>
+                </li>
+                {
+                    pageIds.map((id) => {
+                        return (
+                            <li key={`${id}-post`}>
+                                <Link href={`/posts/${id}`}>Go to: /posts/[id].tsx</Link>
+                            </li>
+                        )
+                    })
+                }
+
+                {
+                    pageIds.map((id) => {
+                        return (
+                            <li key={`${id}-post-comment`}>
+                                <Link href={`/posts/${id}/comment`}>Go to: /posts/[id]/[comment].tsx</Link>
+                            </li>
+                        )
+                    })
+                }
+
+
+            </ul>
+        </>
+    )
+
+}
